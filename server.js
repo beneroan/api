9161f3b0c52c83200b3b195868ad4f15c43813f4
Ben Taylor <bentaylor@bentaylor.me>
2020-04-17 22:21:50 -0500

    random values

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const PORT = 6000;
 
 const getMachineStatus = (callback) => {
-   callback(true);
+   callback(Math.random() > 0.5);
 }
 
 const getFireStatus = (callback) => {
-   callback(false);
+   callback(Math.random() > 0.5);
 }
 
 app.get('/status/machine', (req, res) => {
