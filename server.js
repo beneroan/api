d38bc8e26b41d8c7b9851399382f7e5a382f56b1
Ben Taylor <bentaylor@bentaylor.me>
2020-04-18 11:06:31 -0500

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 5000;
+
+app.use(cors());
 
 const getMachineStatus = (callback) => {
    callback((Math.floor((new Date()) / 5000) % 2) == 0);
