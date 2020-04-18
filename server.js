a0711bd06e8cbae9eac25f32394a2f8ed69b2e1c
Ben Taylor <bentaylor@bentaylor.me>
2020-04-18 14:34:55 -0500

    added reminders

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const cors = require('cors');
 const PORT = 5000;
 const { exec } = require('child_process');
+const request = require('request');
 
 app.use(cors());
 
 const lastFreeDb = {};
 const numbersDb = ['16822131240'];
+const peopleToNumber = {
+   'c7d986e8-f760-4df4-8366-1239fafbd110': 0,
+   '9b91a8e8-9027-4576-ab75-7f8205b4b68e': 0
+}
 
 const enableTexts = false;
 
@@ -25,7 +30,7 @@ const getFireStatus = (callback) => {
 
 const sendText = (number, message) => {
    // we have trial account, so this is the only number that we can use
-   if (number != "16822131240") return;
+   if (!enableTexts || number != "16822131240") return;
 
    exec(`curl 'https://api.twilio.com/2010-04-01/Accounts/AC00c64de50527ef8bc8db6ef5dfcf35e9/Messages.json' -X POST \
          --data-urlencode 'To=+${number}' \
@@ -59,12 +64,21 @@ app.get('/status/fire', (req, res) => {
    });
 });
 
+app.get('/remind', (req, res) => {
+   request.post('http://localhost:5001/identify', {}, (err, res, body) => {
+      console.log(body);
+      const number = peopleToNumber[body.id];
+      sendText(number, `Dont forget your laundry! Someone is waiting for the machine.`);
+   });
+   res.end();
+});
+
 app.listen(PORT, () => {
    console.log(`Listening on ${PORT}`);
 });
 
 if (enableTexts) {
-let lastText = 0;
+   let lastText = 0;
    setInterval(() => {
       getFireStatus((status) => {
          if (status && (new Date() - lastText) > 2 * 60 * 1000) {
