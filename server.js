c8e49877dcef45fe90257dcc91af3ffcf572c770
Ben Taylor <bentaylor@bentaylor.me>
2020-04-18 11:10:36 -0500

    query multiple machines

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const PORT = 5000;
 
 app.use(cors());
 
-const getMachineStatus = (callback) => {
-   callback((Math.floor((new Date()) / 5000) % 2) == 0);
+const getMachineStatus = (id, callback) => {
+   if (!id || id == 'A100Laundy Machine 1') {
+      callback((Math.floor((new Date()) / 5000) % 2) == 0);
+   } else {
+      callback(false);
+   }
 }
 
 const getFireStatus = (callback) => {
@@ -14,7 +18,7 @@ const getFireStatus = (callback) => {
 }
 
 app.get('/status/machine', (req, res) => {
-   getMachineStatus((status) => {
+   getMachineStatus(req.query.wing + req.query.machine, (status) => {
       res.json({
          free: status
       });
